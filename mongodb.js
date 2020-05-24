dd851b54f42854606a637ea97eee0fd80748d0c1
maylenepoulsen <mayleneski@hotmail.com>
2020-05-24 12:24:15 -0500

    find on a collection

--- mongodb.js
+++ mongodb.js
@@ -3,11 +3,6 @@ const { MongoClient, ObjectID } = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID();
-console.log(id.id.length);
-console.log(id.toHexString().length)
-
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to database');
@@ -15,49 +10,19 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
   const db = client.db(databaseName)
 
-    //   db.collection('users').insertOne({
-    //       name: 'Sophie',
-    //       age: 12
-    //   }, (error, result) => {
-    //     if (error) {
-    //       return console.log('Unable to insert user')
-    //     }
-    //     console.log(result.ops)
-    //   })
+  // db.collection('users').findOne( { _id: new ObjectID('5ec99c09088e810e837edb24') }, (error, user) => {
+  //   if (error) {
+  //     return console.log('Unable to fetch user')
+  //   }
 
-    //  db.collection('users').insertMany([
-    //      {
-    //         name: 'Mark',
-    //         age: 42
-    //      },
-    //      {
-    //         name: 'Sydney',
-    //         age: 16 
-    //      }
-    //  ], (error, result) => {
-    //    if (error) {
-    //      return console.log('Unable to insert documents')    
-    //    }
-    //    console.log(result.ops)
-    //  })
+  //   console.log(user)
+  // })
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //       description: 'Order items from discount dance',
-    //       completed: false
-    //     }, {
-    //       description: 'Ordered 2 rompers for Sophie',
-    //       completed: true
-    //     }, {
-    //       description: 'Finish action items on LinkedIn',
-    //       completed: false    
-    //     }
-    // ], (error, result) => {
-    //   if (error) {
-    //     return console.log('Unable to insert documents')
-    //   }
-    //   console.log(result.ops)
-    // })
+  db.collection('users').find( { age: 40 } ).toArray((error, users) => {
+    if (error) {
+      return console.log('Unable to fetch users')
+    }
 
-
+    console.log(users)
+  })
 })
