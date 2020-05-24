d574f9df27d97554945d01a56084937d726123fb
maylenepoulsen <mayleneski@hotmail.com>
2020-05-24 17:15:29 -0500

    mongoose connection

--- mongodb.js
+++ mongodb.js
@@ -9,42 +9,20 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
   }
 
   const db = client.db(databaseName)
-
-  // db.collection('users').findOne( { _id: new ObjectID('5ec99c09088e810e837edb24') }, (error, user) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch user')
-  //   }
-
-  //   console.log(user)
+  
+  // db.collection('users').deleteMany({
+  //   age: 40
+  // }).then((result) => {
+  //   console.log(result)
+  // }).catch((error) => {
+  //   console.log(error)
   // })
 
-  // db.collection('users').find( { age: 40 } ).toArray((error, users) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch users')
-  //   }
-
-  //   console.log(users)
-  // })
-
-  // db.collection('users').find( { age: 40 } ).count((error, count) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch')
-  //   }
-
-  //   console.log(count)
-  // })
-
-  db.collection('tasks').findOne( { _id: new ObjectID('5ec99317970e2d0d136ad172')}, (error, task) => {
-    if (error) {
-      return console.log(error)
-    }
-    console.log(task)
-  })
-
-  db.collection('tasks').find( { completed: false }).toArray((error, tasks) => {
-    if (error) {
-      return console.log('Unable to fetch tasks')
-    }
-    console.log(tasks)
+  db.collection('tasks').deleteOne({
+    description: 'Ordered 2 rompers for Sophie'
+  }).then((result) => {
+    console.log(result)
+  }).catch((error) => {
+    console.log(error)
   })
 })
