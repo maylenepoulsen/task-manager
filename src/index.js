0101fa540383755485527902fe9cde407c1d0555
maylenepoulsen <mayleneski@hotmail.com>
2020-06-03 12:24:21 -0500

    adding auth to user route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//     res.send('GET requests are disabled')
+//   } else {
+//     next()
+//   }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is under maintenance. Please try again soon!')
+// })
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -14,17 +26,14 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
 
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-  const password = 'Red12345!';
-  const hashedPassword = await bcrypt.hash(password, 8)
-
-  console.log(password)
-  console.log(hashedPassword)
-
-  const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-  console.log(isMatch);
+  const token = jwt.sign({ _id: 'abc123'}, 'thisismynewcourse', { expiresIn: '7 days' })
+  console.log(token)
+  
+  const data = jwt.verify(token, 'thisismynewcourse')
+  console.log(data)
 }
 
 myFunction()
