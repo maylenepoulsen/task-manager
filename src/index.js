9f6e05ff9cd6c11096de0144784253c65b2d22e1
maylenepoulsen <mayleneski@hotmail.com>
2020-06-10 16:36:12 -0500

    added user upload avatar using multer and sharp

--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//     res.send('GET requests are disabled')
-//   } else {
-//     next()
-//   }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is under maintenance. Please try again soon!')
-// })
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -25,18 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-//   const task = await Task.findById('5eda8df848ca97094a298ad4')
-//   await task.populate('owner').execPopulate()
-//   console.log(task.owner)
-
-//   const user = await User.findById('5eda8be6ffebc908a9b13807')
-//   await user.populate('tasks').execPopulate()
-//   console.log(user.tasks)
-}
-
-main()
